b28d1e7afdbba89f625ebe3d2aac3ac6f14d9630
Nitin <nsinghboss02@gmail.com>
2021-08-31 22:15:29 +0530

    Initial commit

---
+++ src/com/nitincodes/Main.java
@@ -0,0 +1,12 @@
+package com.nitincodes;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// java program to print diagonal element of an array:
+    int arr[][] = new int[][]{{7,8,9},{2,6,4},{7,3,5}};
+    for(int i=0;i< arr.length;i++){
+        System.out.println(arr[i][i]);
+    }
+    }
+}
